ee6887b699cb31d08ef0e61e1ff30ed61bb11241
chitz <54183293+shubham6350@users.noreply.github.com>
2022-09-21 22:27:35 +0530

    ios configuration completed

--- TODO/ios/TODO/AppDelegate.mm
+++ TODO/ios/TODO/AppDelegate.mm
@@ -1,5 +1,3 @@
-
-#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -7,8 +5,6 @@
 #import <React/RCTRootView.h>
 
 #import <React/RCTAppSetupUtils.h>
-
-
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -36,7 +32,6 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
-  
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
